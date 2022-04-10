7c1415536e0d96f95be1e7b30e0287280fd23869
nromans01 <100789712+nromans01@users.noreply.github.com>
2022-04-09 19:36:23 -0500

    Add files via upload
    Partial code for Balala

---
+++ labor.java
@@ -0,0 +1,96 @@
+if (targetJButtonName.contains("a7"))
+   targetJButton = a7;
+
+else if (targetJButtonName.contains("a5"))
+   targetJButton = a5;
+
+else if (targetJButtonName.contains("a3"))
+   targetJButton = a3;
+
+else if (targetJButtonName.contains("a1"))
+   targetJButton = a1;
+
+else if (targetJButtonName.contains("b8"))
+   targetJButton = b8;
+
+else if (targetJButtonName.contains("b6"))
+   targetJButton = b6;
+
+else if (targetJButtonName.contains("b4"))
+   targetJButton = b4;
+
+else if (targetJButtonName.contains("b2"))
+   targetJButton = b2;
+
+else if (targetJButtonName.contains("c7"))
+   targetJButton = c7;
+   
+else if (targetJButtonName.contains("c5"))
+   targetJButton = c5;
+   
+else if (targetJButtonName.contains("c3"))
+   targetJButton = c3;
+   
+else if (targetJButtonName.contains("c1"))
+   targetJButton = c1;
+   
+else if (targetJButtonName.contains("d8"))
+   targetJButton = d8;
+
+else if (targetJButtonName.contains("d6"))
+   targetJButton = d6;
+   
+else if (targetJButtonName.contains("d4"))
+   targetJButton = d4;
+   
+else if (targetJButtonName.contains("d2"))
+   targetJButton = d2;
+   
+else if (targetJButtonName.contains("e7"))
+   targetJButton = e7;
+   
+else if (targetJButtonName.contains("e5"))
+   targetJButton = e5;
+   
+else if (targetJButtonName.contains("e3"))
+   targetJButton = e3;
+   
+else if (targetJButtonName.contains("e1"))
+   targetJButton = e1;
+   
+else if (targetJButtonName.contains("f8"))
+   targetJButton = f8;
+   
+else if (targetJButtonName.contains("f6"))
+   targetJButton = f6;
+   
+else if (targetJButtonName.contains("f4"))
+   targetJButton = f4;
+
+else if (targetJButtonName.contains("f2"))
+   targetJButton = f2;
+   
+else if (targetJButtonName.contains("g7"))
+   targetJButton = g7;
+   
+else if (targetJButtonName.contains("g5"))
+   targetJButton = g5;
+   
+else if (targetJButtonName.contains("g3"))
+   targetJButton = g3;
+   
+else if (targetJButtonName.contains("g1"))
+   targetJButton = g1;
+   
+else if (targetJButtonName.contains("h8"))
+   targetJButton = h8;
+   
+else if (targetJButtonName.contains("h6"))
+   targetJButton = h6;
+   
+else if (targetJButtonName.contains("h4"))
+   targetJButton = h4;
+   
+else if (targetJButtonName.contains("h2"))
+   targetJButton = h2;
+   
